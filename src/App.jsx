ad3d63197d569314fa82ee397b3f1b9726064c81
Damarreindra <damarihya08@gmail.com>
2024-11-21 04:09:43 +0700

    fix endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
  return(
  <Router>
   <Routes>
+  <Route path="/" element={<DashboardPage/>}/>
     <Route path="/dashboard" element={<DashboardPage/>}/>
     <Route path="/dashboard/product/bank-product" element={<BankProduct />} />
     <Route path="/dashboard/product/bank-product/:slug/detail" element={<DetailBankProduct />} />
